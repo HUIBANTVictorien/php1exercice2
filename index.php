20740eee9eb00b7aaa9ce6918bbafe77ca1c6dae
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 16:04:25 +0100

    maj exo2

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<?php
-$firstname = 'HUIBANT';
-$lastname = 'Victorien';
-$age = 25;
-echo $firstname.'<br/>';
-echo $lastname.'<br/>';
-echo $age;
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 2 partie 1 php</title>
+  </head>
+  <body>
+    <?php
+    $firstname = 'HUIBANT';
+    $lastname = 'Victorien';
+    $age = 25;
+    echo $firstname.'<br/>';
+    echo $lastname.'<br/>';
+    echo $age;
+    ?>
+  </body>
+</html>
